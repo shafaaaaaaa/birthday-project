b73192c29379ef869d42bf86f0b46e770b07e25d
shafa <izzatishafa2@gmail.com>
2024-05-26 18:44:15 +0700

    updated

--- src/Pages/Confirm.js
+++ src/Pages/Confirm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "../App.css"
 
 const Confirm = () => {
   const navigate = useNavigate();
@@ -11,10 +12,10 @@ const Confirm = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-center gap-4">
-        <p className="text-xl font-bold">Are You Ready?</p>
+        <p className="font-mono text-xl font-extrabold">Are You Ready?</p>
         <a
           onClick={handleClick}
-          className="bg-sky-950 hover:bg-sky-900 text-md text-white px-5 py-1 border border-none rounded-full cursor-pointer"
+          className="font-sans font-semibold bg-sky-950 hover:bg-sky-900 text-md text-white px-5 py-1 border border-none rounded-full cursor-pointer shadow shadow-3xl"
         >
           Yes
         </a>
