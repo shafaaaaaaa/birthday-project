0ebba81fe95df8806107934cca97207db1fc0604
shafa <izzatishafa2@gmail.com>
2024-05-27 10:31:03 +0700

    updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import LazyLoad from "react-lazyload";
 import "../App.css";
 import Doll3 from "../Assets/cc19a9b4-8ef1-4d04-836f-2c883c14863f.jpeg";
 import AudioFile from "../Assets/selamat-ulang-tahun.mp3";
@@ -37,13 +38,22 @@ const Home = () => {
     };
   }, []);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   if (audioRef.current) {
+  //     audioRef.current.play().catch((error) => {
+  //       console.log("Audio playback failed:", error);
+  //     });
+  //   }
+  // }, []);
+
+  const playAudio = () => {
     if (audioRef.current) {
       audioRef.current.play().catch((error) => {
         console.log("Audio playback failed:", error);
       });
     }
-  }, []);
+  };
+  
 
   return (
     <div className="main-container">
@@ -54,13 +64,18 @@ const Home = () => {
       )}
       <div className="card">
         <section className="section1">
-          <img src={Doll3} alt="birthday" className="picture" />
-          <audio
-            ref={audioRef}
-            src={AudioFile}
-            controls
-            className="audio"
-          ></audio>
+          <LazyLoad height={200} offset={100}>
+            <img src={Doll3} alt="birthday" className="picture" />
+          </LazyLoad>
+          <LazyLoad height={200} offset={100}>
+            <audio
+              ref={audioRef}
+              src={AudioFile}
+              controls
+              className="audio"
+              onLoadedData={playAudio}
+            ></audio>
+          </LazyLoad>
         </section>
         <section className="p-10">
           <div className="heading">
@@ -68,14 +83,14 @@ const Home = () => {
             <p className="text-lg">Happy 18!</p>
           </div>
           <p className="wishes-text">
-            Wishing you a birthday filled with things that make you
-            smile, Adrian. I wish you always be healthy, Allah makes all
-            your affairs easy, & you will be given the strength and enthusiasm
-            to continue to pursue your dreams and overcome all the challenges
-            that come your way. Don't ever get tired of the process, one day you
-            will get there. <br /> By the way, we are the same age now!! wkwk,
-            all the best for you yaa, Thank you for taking the time to read this
-            to the end.
+            Wishing you a birthday filled with things that make you smile,
+            Adrian. I wish you always be healthy, Allah makes all your affairs
+            easy, & you will be given the strength and enthusiasm to continue to
+            pursue your dreams and overcome all the challenges that come your
+            way. Don't ever get tired of the process, one day you will get
+            there. <br /> By the way, we are the same age now!! wkwk, all the
+            best for you yaa, Thank you for taking the time to read this to the
+            end.
           </p>
           <p className="signature">
             Sweet Regards, Izzati
